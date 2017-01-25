66970f32aae04e680df8d5c2adb8d50b9ceec8d3
Arthur Dingemans <arthur@argu.co>
2017-01-25 10:02:40 +0100

    [IMP] Put use_transactional_fixtures back into spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,6 +22,8 @@ RSpec.configure do |config|
   config.include ServiceBase::TestHelpers::RequestHelpers
   config.include TestMocks
 
+  config.use_transactional_fixtures = true
+
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 
