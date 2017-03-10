9648b7330d240ae7fcb201b593b15281d92ec7d8
Arthur Dingemans <arthur@argu.co>
2017-03-10 12:22:07 +0100

    [SM-2] Add health_check initializer
    The services should add the health_check gem and include
      constraints(Argu::WhitelistConstraint) do
        health_check_routes
      end

---
+++ config/initializers/health_check.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+HealthCheck.setup do |config|
+  # uri prefix (no leading slash)
+  config.uri = 'd/health'
+
+  # Text output upon success
+  config.success = 'success'
+
+  # Timeout in seconds used when checking smtp server
+  config.smtp_timeout = 30.0
+
+  # http status code used when plain text error message is output
+  # Set to 200 if you want your want to distinguish between partial (text does not include success) and
+  # total failure of rails application (http status of 500 etc)
+
+  config.http_status_for_error_text = 500
+
+  # http status code used when an error object is output (json or xml)
+  # Set to 200 if you want your want to distinguish between partial (healthy property == false) and
+  # total failure of rails application (http status of 500 etc)
+
+  config.http_status_for_error_object = 500
+
+  # You can customize which checks happen on a standard health check, eg to set an explicit list use:
+  config.standard_checks = %w(database migrations redis custom)
+
+  # Add one or more custom checks that return a blank string if ok, or an error message if there is an error
+  config.add_custom_check do
+    # Check RabbitMQ connection
+    Connection.new.connection.close && ''
+  end
+
+  # max-age of response in seconds
+  # cache-control is public when max_age > 1 and basic_auth_username is not set
+  # You can force private without authentication for longer max_age by
+  # setting basic_auth_username but not basic_auth_password
+  config.max_age = 1
+
+  # http status code used when the ip is not allowed for the request
+  config.http_status_for_ip_whitelist_error = 403
+end
