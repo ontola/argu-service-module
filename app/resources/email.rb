9fa55ad81d70b684a7002e9cd1f2674e229def50
Arthur Dingemans <arthur@argu.co>
2017-03-20 14:15:16 +0100

    [IMP] Add Email resource

---
+++ app/resources/email.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+class Email < ActiveResourceModel
+  def self.collection_name
+    'email/emails'
+  end
+
+  %w(opened accepted rejected delivered failed clicked unsubscribed complained stored).each do |event|
+    define_method "#{event}?" do
+      email_events.any? { |e| e.event == event }
+    end
+  end
+
+  def status
+    return 'delivered' if delivered?
+    'failed' if failed?
+  end
+end
--- lib/json_api_collection_parser.rb
+++ lib/json_api_collection_parser.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
 class JsonApiCollectionParser < ActiveResource::Collection
   def initialize(elements = [])
     @elements = if elements.is_a?(Hash)
-                  elements['data'].map { |record| {'data' => record, 'included' => elements['included']}}
+                  elements['data'].map { |record| {'data' => record, 'included' => elements['included']} }
                 else
                   elements
                 end
