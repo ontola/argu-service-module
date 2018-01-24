c5ea4e640fa9efae3e0bde8b024ec00f343ac347
Arthur Dingemans <arthur@argu.co>
2018-01-24 08:58:53 +0100

    [SM][ARGU-929] Fix healthcheck
    The upgrade to health_check 2.7.0 requires to manually set the redis url when it differs from localhost

--- config/initializers/health_check.rb
+++ config/initializers/health_check.rb
@@ -39,4 +39,6 @@ HealthCheck.setup do |config|
 
   # http status code used when the ip is not allowed for the request
   config.http_status_for_ip_whitelist_error = 403
+
+  config.redis_url = ENV['REDIS_URL']
 end
