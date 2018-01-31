c636389e5fe567d90dd7f6ba4aa00827590b5a6a
Arthur Dingemans <arthur@argu.co>
2018-01-31 09:15:49 +0100

    [SM][ARGU-1354] Add uri templates for action menu items

--- app/helpers/uri_template_helper.rb
+++ app/helpers/uri_template_helper.rb
@@ -5,6 +5,7 @@ module UriTemplateHelper
     tmpl = uri_template(template)
     raise "Uri template #{template} is missing" unless tmpl
     args[:parent_iri] = split_iri_segments(args[:parent_iri]) if args[:parent_iri].present?
+    args[:collection_iri] = split_iri_segments(args[:collection_iri]) if args[:collection_iri].present?
     args[:only_path] ? tmpl.expand(args) : "https://#{Rails.application.config.host_name}#{tmpl.expand(args)}"
   end
 
