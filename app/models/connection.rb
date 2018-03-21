16ac51885c009f5d26cc064fecf0a41ead07db09
Arthur Dingemans <arthur@argu.co>
2018-03-21 09:43:49 +0100

    [SM][DP-12] Restore database connections in subscribe task

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -12,6 +12,12 @@ class Connection
     @connection = connection
   end
 
+  def self.process_event(delivery_info, body)
+    puts " [x] #{delivery_info.delivery_tag}:#{JSON.parse(body)}"
+    ActiveRecord::Base.connection.reconnect! unless ActiveRecord::Base.connection.active?
+    DataEvent.parse(body)
+  end
+
   # @param [String] name The name of the channel to publish to
   # @param [String] body The string to publish
   def publish(name, body)
@@ -33,8 +39,7 @@ class Connection
     channel, q = subscribe_to_queue(queue)
     begin
       q.subscribe(manual_ack: true, block: true) do |delivery_info, _properties, body|
-        puts " [x] #{delivery_info.delivery_tag}:#{JSON.parse(body)}"
-        yield DataEvent.parse(body)
+        yield process_event(delivery_info, body)
         channel.ack(delivery_info.delivery_tag)
       end
     rescue Interrupt
@@ -45,6 +50,7 @@ class Connection
   def self.subscribe_to_queue(queue)
     channel = new.connection.create_channel
     channel.on_uncaught_exception do |e, _consumer|
+      puts "error: #{e}"
       Bugsnag.notify(e)
     end
     x = channel.fanout('events', durable: true)
