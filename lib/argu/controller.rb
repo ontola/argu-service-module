25760911a0458603d5bee3193d60c8f84cbf7dfb
Arthur Dingemans <arthur@argu.co>
2018-03-23 16:11:06 +0100

    [SM-24] Move basic reponse logic to service_module

--- lib/argu/controller.rb
+++ lib/argu/controller.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
+require 'argu/controller/common'
 require 'argu/controller/error_handling'
---
+++ lib/argu/controller/common.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+require 'argu/controller/common/responses'
