0d374814dd9687534fd30443766a9e7bbccc374a
Arthur Dingemans <arthur@argu.co>
2018-04-16 10:42:20 +0200

    [SM][ARGU-1439] Add fragment to argu_url

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -4,6 +4,7 @@ module UrlHelper
   def argu_url(path = '', params = {})
     url = URI(Rails.application.config.origin)
     url.path = path
+    url.fragment = params.delete(:fragment)
     url.query = params.to_param if params.present?
     url.to_s
   end
