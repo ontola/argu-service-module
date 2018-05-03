d8380f3b4ecc2e8ccd73da83049f24533866b1ed
Arthur Dingemans <arthur@argu.co>
2018-05-03 13:51:26 +0200

    [SM][ARGU-1451] Add more iri helper methods

--- app/helpers/uri_template_helper.rb
+++ app/helpers/uri_template_helper.rb
@@ -1,6 +1,23 @@
 # frozen_string_literal: true
 
 module UriTemplateHelper
+  # @return [RDF::URI]
+  def collection_iri(parent, type, opts = {})
+    canonical = opts.delete(:canonical)
+    RDF::URI(
+      expand_uri_template(
+        "#{type}_collection_#{canonical ? 'canonical' : 'iri'}",
+        opts.merge(parent_iri: parent.try(:iri_path) || parent)
+      )
+    )
+  end
+
+  # @return [String]
+  def collection_iri_path(parent, type, opts = {})
+    collection_iri(parent, type, opts.merge(only_path: true)).to_s
+  end
+
+  # @return [String]
   def expand_uri_template(template, args = {})
     tmpl = uri_template(template)
     raise "Uri template #{template} is missing" unless tmpl
@@ -10,15 +27,51 @@ module UriTemplateHelper
     args[:only_path] ? path : path_with_hostname(path)
   end
 
+  def link_to(name = nil, options = nil, html_options = nil, &block)
+    name = name.try(:iri_path) || name
+    options = options.try(:iri_path) || options
+    super
+  end
+
+  # @return [String]
   def path_with_hostname(path)
     "#{Rails.application.config.origin}#{path}"
   end
 
+  # @return [Array<String>]
   def split_iri_segments(iri)
     iri.to_s.split('/').map(&:presence).compact
   end
 
+  # @return [URITemplat]
   def uri_template(template)
     Rails.application.config.uri_templates[template]
+  end
+
+  # @return [RDF::URI]
+  def new_iri(parent, collection = nil, opts = {})
+    query = opts.delete(:query)
+    iri = parent.is_a?(String) ? parent : collection_iri_path(parent, collection, opts)
+    uri = RDF::URI(expand_uri_template(:new_iri, opts.merge(parent_iri: iri)))
+    uri.query = query.to_param if query
+    uri
+  end
+
+  # @return [String]
+  def new_iri_path(parent, collection = nil, opts = {})
+    new_iri(parent, collection, opts.merge(only_path: true)).to_s
+  end
+
+  %i[edit delete trash untrash move settings statistics feeds conversions invites].each do |method|
+    # @return [RDF::URI]
+    define_method "#{method}_iri" do |parent, opts = {}|
+      iri = parent.try(:iri_path) || parent
+      RDF::URI(expand_uri_template("#{method}_iri", opts.merge(parent_iri: iri)))
+    end
+
+    # @return [String]
+    define_method "#{method}_iri_path" do |parent, opts = {}|
+      send("#{method}_iri", parent, opts.merge(only_path: true)).to_s
+    end
   end
 end
