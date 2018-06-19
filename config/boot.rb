a45dafd23dd73af13c22658e803b78f194e0e105
Arthur Dingemans <arthur@argu.co>
2018-06-19 16:34:49 +0200

    [SM][ARGU-1483] Add bootsnap to boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -3,3 +3,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__.gsub('service_module/', ''))
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'bootsnap/setup'
