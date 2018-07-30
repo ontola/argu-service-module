b0c4a15c10006b95f3575d00bf9b68277a6d0a95
Arthur Dingemans <arthur@argu.co>
2018-07-30 11:31:11 +0200

    [SM][ARGU-1503] Cache iri path

--- app/models/concerns/iriable.rb
+++ app/models/concerns/iriable.rb
@@ -3,39 +3,53 @@
 module Iriable
   extend ActiveSupport::Concern
 
-  included do
-    include UriTemplateHelper
+  include UriTemplateHelper
 
-    # The canonical IRI of the object. The used URL may differ.
-    # @return [RDF::URI] IRI of the object.
-    def canonical_iri(only_path: false)
-      return iri(only_path: only_path) if uri_template("#{model_name.route_key}_canonical_iri").blank?
-      RDF::URI(
-        expand_uri_template("#{model_name.route_key}_canonical_iri", **canonical_iri_opts.merge(only_path: only_path))
-      )
-    end
+  # The canonical IRI of the object. The used URL may differ.
+  # @return [RDF::URI] IRI of the object.
+  def canonical_iri(only_path: false)
+    return iri(only_path: only_path) if uri_template("#{model_name.route_key}_canonical_iri").blank?
+    RDF::URI(
+      expand_uri_template("#{model_name.route_key}_canonical_iri", **canonical_iri_opts.merge(only_path: only_path))
+    )
+  end
 
-    def canonical_iri_opts
-      {id: id, :"#{self.class.name.underscore}_id" => id}
-    end
+  def canonical_iri_opts
+    {id: id, :"#{self.class.name.underscore}_id" => id}
+  end
 
-    # The IRI of the object. The used URL may differ.
-    # @return [RDF::URI] IRI of the object.
-    def iri(opts = {})
-      RDF::URI(expand_uri_template("#{model_name.route_key}_iri", **iri_opts.merge(opts)))
-    end
+  # The IRI of the object. The used URL may differ.
+  # @return [RDF::URI] IRI of the object.
+  def iri(opts = {})
+    iri_from_cache(opts) || iri_from_template(opts)
+  end
 
-    def iri_opts
-      {id: id, :"#{self.class.name.underscore}_id" => id}
-    end
+  def iri_from_cache(opts)
+    return if opts.except(:only_path).present? || !persisted? || !has_attribute?(:iri_cache)
+    path = iri_cache || cache_iri!
+    RDF::URI(opts[:only_path] ? path : path_with_hostname(path))
+  end
 
-    # @return [String]
-    def iri_path(opts = {})
-      iri = iri(opts)
-      iri.scheme = nil
-      iri.authority = nil
-      iri.to_s
-    end
+  def iri_from_template(opts)
+    RDF::URI(expand_uri_template("#{model_name.route_key}_iri", **iri_opts.merge(opts)))
+  end
+
+  def iri_opts
+    {id: id, :"#{self.class.name.underscore}_id" => id}
+  end
+
+  # @return [String]
+  def iri_path(opts = {})
+    iri = iri(opts)
+    iri.scheme = nil
+    iri.authority = nil
+    iri.to_s
+  end
+
+  def cache_iri!
+    return unless has_attribute?(:iri_cache) && persisted?
+    update_column(:iri_cache, iri_from_template(only_path: true).to_s)
+    iri_from_template(only_path: true)
   end
 
   module ClassMethods
