d9068b758ca4706ec74664c5eed9cdb0de1dbd8e
Arthur Dingemans <arthur@argu.co>
2018-08-14 17:07:32 +0200

    [SM][ARGU-1531] Try to get the table from the association reflection

--- lib/rails_ld/collection_view.rb
+++ lib/rails_ld/collection_view.rb
@@ -50,7 +50,11 @@ module RailsLD
     private
 
     def arel_table
-      @arel_table ||= Arel::Table.new(association_class.to_s.tableize)
+      @arel_table ||= Arel::Table.new(association_table)
+    end
+
+    def association_table
+      parent&.class&.reflect_on_association(collection.association)&.table_name || association_class.to_s.tableize
     end
 
     def base_count
