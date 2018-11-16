7d3b47dea7e704efe40f93bf07dadb7416ce90c7
Arthur Dingemans <arthur@argu.co>
2018-11-16 10:05:56 +0100

    [SM-28] Add more info for rabbitmq errors

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -12,12 +12,6 @@ class Connection
     @connection = connection
   end
 
-  def self.process_event(delivery_info, body)
-    puts " [x] #{delivery_info.delivery_tag}:#{JSON.parse(body)}"
-    ActiveRecord::Base.connection.reconnect! unless ActiveRecord::Base.connection.active?
-    DataEvent.parse(body)
-  end
-
   # @param [String] name The name of the channel to publish to
   # @param [String] body The string to publish
   def publish(name, body)
@@ -30,39 +24,66 @@ class Connection
     connection.close
   end
 
-  def self.publish(name, body)
-    new.publish(name, body)
-    body
-  end
-
-  def self.subscribe(queue)
-    channel, q = subscribe_to_queue(queue)
-    begin
-      q.subscribe(manual_ack: true, block: true) do |delivery_info, _properties, body|
-        yield process_event(delivery_info, body)
-        channel.ack(delivery_info.delivery_tag)
-      end
-    rescue Interrupt
-      connection.close
-    end
-  end
-
-  def self.subscribe_to_queue(queue)
-    channel = new.connection.create_channel
-    channel.on_uncaught_exception do |e, _consumer|
-      puts "error: #{e}"
-      Bugsnag.notify(e)
-    end
-    x = channel.fanout('events', durable: true)
-    q = channel.queue(queue, durable: true)
-    q.bind(x)
-    puts ' [*] Waiting for events. To exit press CTRL+C'
-    [channel, q]
-  end
-
   private
 
   def with_channel
     connection.with_channel { |c| yield c }
+  end
+
+  class << self
+    def process_event(delivery_info, body)
+      puts " [x] #{delivery_info.delivery_tag}:#{JSON.parse(body)}"
+      ActiveRecord::Base.connection.reconnect! unless ActiveRecord::Base.connection.active?
+      DataEvent.parse(body)
+    end
+
+    def publish(name, body)
+      new.publish(name, body)
+      body
+    end
+
+    def subscribe(queue_name)
+      channel, queue = subscribe_to_queue(queue_name)
+      begin
+        listen(channel, queue)
+      rescue Interrupt
+        connection.close
+      end
+    end
+
+    def subscribe_to_queue(queue)
+      channel = new.connection.create_channel
+      channel.on_uncaught_exception do |e, _consumer|
+        puts "error: #{e}"
+        Bugsnag.notify(e) { |report| add_rabbit_mq_info(report) }
+      end
+      x = channel.fanout('events', durable: true)
+      q = channel.queue(queue, durable: true)
+      q.bind(x)
+      [channel, q]
+    end
+
+    private
+
+    def add_rabbit_mq_info(report)
+      report.add_tab(
+        :rabbitmq,
+        delivery_info: @delivery_info.to_s,
+        properties: @properties,
+        body: @body
+      )
+    end
+
+    def listen(channel, queue)
+      puts ' [*] Waiting for events. To exit press CTRL+C'
+
+      queue.subscribe(manual_ack: true, block: true) do |delivery_info, properties, body|
+        @delivery_info = delivery_info
+        @properties = properties
+        @body = body
+        yield process_event(delivery_info, body)
+        channel.ack(delivery_info.delivery_tag)
+      end
+    end
   end
 end
