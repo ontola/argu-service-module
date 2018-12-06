2e05e012ef62873cbcd8f969994151de0c0b230d
Arthur Dingemans <arthur@argu.co>
2018-12-06 16:06:46 +0100

    [SM][TS-55] Add regex helper to service module

---
+++ app/helpers/regex_helper.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module RegexHelper
+  SINGLE_EMAIL = /
+  #{RFC822::Patterns::LOCAL_PT}
+    \x40
+    (?:(?:#{URI::REGEXP::PATTERN::DOMLABEL}\.)+#{URI::REGEXP::PATTERN::TOPLABEL}\.?)+#{RFC822::Patterns::ATOM}
+  /x
+  EMAIL = /\A#{SINGLE_EMAIL}\z/
+end
