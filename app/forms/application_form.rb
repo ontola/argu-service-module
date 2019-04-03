902a33ee4ace4e7b0d8dec3aceedfcde24d5127d
Arthur Dingemans <arthur@argu.co>
2019-04-03 13:29:23 +0200

    [SM][ARGU-1779] Move Tableable and ApplicationForm to service module

---
+++ app/forms/application_form.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class ApplicationForm < RailsLD::Form
+  class << self
+    private
+
+    def actor_selector
+      {
+        custom: true,
+        datatype: NS::XSD[:string],
+        default_value: ->(resource) { resource.form.user_context.user.iri },
+        max_count: 1,
+        sh_in: ->(resource) { actors_iri(resource.form.target.root) }
+      }
+    end
+  end
+end
