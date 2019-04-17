bdd1228265b2dd2eb8d2aacc08550d98ef70495f
Arthur Dingemans <arthur@argu.co>
2019-04-17 12:31:13 +0200

    [SM][ARGU-1827] Remove unused method from CollectionView

--- app/models/collection_view.rb
+++ app/models/collection_view.rb
@@ -1,9 +1,3 @@
 # frozen_string_literal: true
 
-class CollectionView < RailsLD::CollectionView
-  def preload_singular_association(members, key, reflection, _includes)
-    super
-    return unless reflection.klass <= Edge && key != :root
-    members.each { |member| member.send(key).association(:root).target = member.root }
-  end
-end
+class CollectionView < RailsLD::CollectionView; end
