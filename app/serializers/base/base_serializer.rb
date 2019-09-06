cc1886b4112519cd29320fd9d60f5926dc08c71c
Arthur Dingemans <arthur@argu.co>
2019-09-06 11:57:36 +0200

    [SM][RIVM-2] Allow array enum attributes

--- app/serializers/base/base_serializer.rb
+++ app/serializers/base/base_serializer.rb
@@ -30,4 +30,23 @@ class BaseSerializer < ActiveModel::Serializer
   def tenant
     object.forum.url if object.respond_to? :forum
   end
+
+  class << self
+    # overwrite of LinkedRails::Serializer to enable arrays
+    def enum(key, opts = nil) # rubocop:disable Metrics/AbcSize
+      self._enums ||= {}
+      self._enums[key] = opts
+
+      define_method(key) do
+        enum_opts = self.class.enum_options(key).try(:[], :options)
+        return if enum_opts.blank?
+
+        raw_value = object.send(key)
+        return if raw_value.blank?
+        return raw_value.map { |v| enum_opts[v.to_sym].try(:[], :iri) } if raw_value.is_a?(Array)
+
+        enum_opts[raw_value&.to_sym].try(:[], :iri)
+      end
+    end
+  end
 end
