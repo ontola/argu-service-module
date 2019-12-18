ece5e56613c3bf481677e36b932cdaa5411f0cc2
Arthur Dingemans <arthur@argu.co>
2019-12-18 08:56:01 +0100

    [SM][ARGU-1970] Image serialization moved to LinkedRails

--- app/serializers/entry_point_serializer.rb
+++ app/serializers/entry_point_serializer.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 class EntryPointSerializer < LinkedRails::EntryPointSerializer
-  has_one :serialized_image, predicate: NS::SCHEMA[:image]
-
-  def image; end
-
-  def serialized_image
-    serialize_image(object.image)
-  end
 end
