1ee253baf96191a90a205d86c7ce062944cadd35
Arthur Dingemans <arthur@argu.co>
2020-01-03 08:49:21 +0100

    [SM][ARGU-1982] Add parent iri to collections iri template

--- app/serializers/collection_serializer.rb
+++ app/serializers/collection_serializer.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class CollectionSerializer < LinkedRails::CollectionSerializer
+  def iri_template
+    object.iri_template.to_s.gsub('{/parent_iri*}', object.parent&.iri || ActsAsTenant.current_tenant.iri)
+  end
 end
