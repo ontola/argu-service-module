80cc9b2adba74bef7ec3995d8da35de8e7462352
Arthur Dingemans <arthur@argu.co>
2020-02-14 14:22:54 +0100

    [SM][ARGU-2008] Fix iris of active resources

--- app/resources/current_user.rb
+++ app/resources/current_user.rb
@@ -57,7 +57,7 @@ class CurrentUser
 
       attrs[:email] = user_data['email']
       attrs[:id] = user_data['id']
-      attrs[:iri] = user_data['@id'] && RDF::DynamicURI(user_data['@id'])
+      attrs[:iri] = user_data['@id'] && RDF::URI(user_data['@id'])
       attrs[:type] = user_data['type']
       attrs[:language] = user_data['language']
       attrs
