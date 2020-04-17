74c47f1b67b0fd427ce50554e435dca2272b6f7d
Arthur Dingemans <arthur@argu.co>
2020-04-17 08:58:08 +0200

    core#270 Inherit CollectionFilterSerializer from LinkedRails

--- app/serializers/collection_filter_serializer.rb
+++ app/serializers/collection_filter_serializer.rb
@@ -1,6 +1,3 @@
 # frozen_string_literal: true
 
-class CollectionFilterSerializer < BaseSerializer
-  attribute :key, predicate: NS::ONTOLA[:filterKey]
-  attribute :value, predicate: NS::ONTOLA[:filterValue]
-end
+class CollectionFilterSerializer < LinkedRails::Collection::FilterSerializer; end
