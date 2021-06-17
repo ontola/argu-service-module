33464d1e700610d9f8ebca28fe8844a3a31447dc
Arthur Dingemans <arthur@argu.co>
2021-06-17 09:15:57 +0200

    core#738 Stop overwriting #iri

--- app/models/collection_filter.rb
+++ app/models/collection_filter.rb
@@ -1,9 +1,3 @@
 # frozen_string_literal: true
 
-class CollectionFilter < LinkedRails::Collection::Filter
-  include LinkedRails::Model
-
-  def iri(_opts = {})
-    self
-  end
-end
+class CollectionFilter < LinkedRails::Collection::Filter; end
