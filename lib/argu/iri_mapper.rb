6fa47ee259c9a2f7707678048f371810cf091c08
Arthur Dingemans <arthur@argu.co>
2022-01-26 16:24:41 +0100

    core#1010 Fix iriPrefix url sanitization

--- lib/argu/iri_mapper.rb
+++ lib/argu/iri_mapper.rb
@@ -9,13 +9,13 @@ module Argu
 
       private
 
-      def sanitized_path(iri) # rubocop:disable Metrics/AbcSize
+      def sanitized_path(iri) # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity
         iri.path = "#{iri.path}/" unless iri.path.ends_with?('/')
         tenant_path = ActsAsTenant.current_tenant&.iri&.path
         uri = tenant_path.present? ? iri.to_s.split("#{ActsAsTenant.current_tenant.iri.path}/").last : iri
         path = URI(uri).path
         iri_suffix = Rails.application.config.try(:iri_suffix)
-        return path if iri_suffix.blank? || path.start_with?(iri_suffix)
+        return path if iri_suffix.blank? || path.start_with?(iri_suffix) || path.start_with?("/#{iri_suffix}")
 
         [iri_suffix, path.delete_prefix('/')].join('/')
       end
