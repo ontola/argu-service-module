74f85746e4d33222e0253ab0b142cdcde438612c
Arthur Dingemans <arthur@argu.co>
2022-02-02 09:46:45 +0100

    core#12 Skip scoping verification for HEAD requests

--- lib/argu/controller/authorization.rb
+++ lib/argu/controller/authorization.rb
@@ -49,7 +49,7 @@ module Argu
       end
 
       def verify_policy_scoped?
-        action_name == 'index'
+        action_name == 'index' && request.method != 'HEAD'
       end
 
       module ClassMethods
