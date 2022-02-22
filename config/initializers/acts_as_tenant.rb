26ad343c22984d7f542e746b4806609237ef5a3c
Arthur Dingemans <arthur@argu.co>
2022-02-22 14:32:30 +0100

    core#2 Always switch tenant again. Was causing weird issues

--- config/initializers/acts_as_tenant.rb
+++ config/initializers/acts_as_tenant.rb
@@ -2,7 +2,7 @@
 
 module ActsAsTenant
   def self.current_tenant=(tenant)
-    if tenant&.database_schema && tenant.database_schema != Apartment::Tenant.current
+    if tenant&.database_schema
       Tenant.create(tenant.database_schema) unless ApplicationRecord.connection.schema_exists?(tenant.database_schema)
 
       Apartment::Tenant.switch!(tenant.database_schema)
