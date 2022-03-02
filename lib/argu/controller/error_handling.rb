328f69309fc487f4bcb22b723873f1f58020385a
Arthur Dingemans <arthur@argu.co>
2022-03-02 15:50:29 +0100

    Fix error codes

--- lib/argu/controller/error_handling.rb
+++ lib/argu/controller/error_handling.rb
@@ -14,6 +14,28 @@ module Argu
       included do
         rescue_from OAuth2::Error, with: :handle_oauth_error
       end
+
+      module ClassMethods
+        def error_status_codes # rubocop:disable Metrics/MethodLength
+          @error_status_codes ||= {
+            'ActionController::ParameterMissing' => 422,
+            'ActionController::RoutingError' => 404,
+            'ActionController::UnpermittedParameters' => 422,
+            'ActiveRecord::RecordNotFound' => 404,
+            'ActiveRecord::RecordNotUnique' => 304,
+            'Doorkeeper::Errors::InvalidGrantReuse' => 422,
+            'LinkedRails::Auth::Errors::Expired' => 410,
+            'LinkedRails::Auth::Errors::Unauthorized' => 401,
+            'Pundit::NotAuthorizedError' => 403,
+            'Argu::Errors::Forbidden' => 403,
+            'Argu::Errors::Unauthorized' => 401,
+            'Argu::Errors::WrongPassword' => 401,
+            'Argu::Errors::UnknownEmail' => 401,
+            'Argu::Errors::AccountLocked' => 401,
+            'Argu::Errors::NoPassword' => 401
+          }
+        end
+      end
     end
   end
 end
