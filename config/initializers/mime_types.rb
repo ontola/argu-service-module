95e313903e0b0a4935beb382382c4f8dcf3f3147
Arthur Dingemans <arthur@argu.co>
2022-05-05 10:03:59 +0200

    core#1 Add empjson mimetype

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,4 @@
 Mime::Type.register 'text/html', :html
 Mime::Type.register 'application/vnd.api+json', :json_api
 Mime::Type.register 'application/hex+x-ndjson', :hndjson
+Mime::Type.register 'application/empathy+json', :empjson
